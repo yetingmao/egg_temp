da36f5ec5eff8b48fc01a0d8c35cc63c4238742b
yetingmao <yetingmao@163.com>
2022-06-21 13:43:07 +0800

    合并

--- app/plugin/selenium/pages/resouces/expert/index.js
+++
@@ -1,8 +0,0 @@
-const { resourcesExpertExpert } = require('./expert');
-const { resourcesExpertSpecialty } = require('./specialty');
-const { resourcesExpertRecommend } = require('./recommend');
-
-
-module.exports = {
-    resourcesExpertExpert, resourcesExpertSpecialty, resourcesExpertRecommend
-}
